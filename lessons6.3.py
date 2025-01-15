423bbbe05570531031065ff5e43064ab88dcb644
SvetlanaNesmiyan <163138406+SvetlanaNesmiyan@users.noreply.github.com>
2025-01-15 22:05:52 +0000

    lessons6.3.py

--- lessons6.3.py
+++ lessons6.3.py
@@ -1,16 +1,10 @@
 def multiply_digits(number):
+    while number > 9:
+        product = 1
+        for digit in str(number):
+            product *= int(digit)
+        number = product
+    return number
 
-  while number > 9:
-    product = 1
-    while number > 0:
-      digit = number % 10
-      product *= digit
-      number //= 10
-    number = product
-  return number
-
-num = int(input("Введіть ціле число:"))
-
-result = multiply_digits(num)
-
-print(result)
+user_input = int(input("Введіть ціле число: "))
+print(multiply_digits(user_input))
