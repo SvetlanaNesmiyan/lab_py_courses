53d6328913a02cf65368dd96097744bea56b4c69
SvetlanaNesmiyan <163138406+SvetlanaNesmiyan@users.noreply.github.com>
2025-01-19 14:20:28 +0000

    lessons7.4.py

---
+++ lessons7.4.py
@@ -0,0 +1,8 @@
+def common_elements():
+    multiples_of_3 = {x for x in range(100) if x % 3 == 0}
+    multiples_of_5 = {x for x in range(100) if x % 5 == 0}
+
+    return multiples_of_3 & multiples_of_5
+
+result = common_elements()
+print(result)
