4b98020dc1245da46f851c7db73730d51bcbf67e
SvetlanaNesmiyan <163138406+SvetlanaNesmiyan@users.noreply.github.com>
2025-01-22 21:12:18 +0000

    lessons8.2.py

---
+++ lessons8.2.py
@@ -0,0 +1,12 @@
+import string
+
+def is_palindrome(text):
+    text = text.lower()
+
+    filtered_text = ''.join(ch for ch in text if ch.isalnum())
+
+    return filtered_text == filtered_text[::-1]
+
+result = is_palindrome('A man, a plan, a canal: Panama')
+
+print(result)
