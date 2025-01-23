32528c007583ff1ffaf43e899ce68a81df73bfb2
SvetlanaNesmiyan <163138406+SvetlanaNesmiyan@users.noreply.github.com>
2025-01-23 13:04:57 +0000

    lessons8.1.py

--- lessons8.1.py
+++ lessons8.1.py
@@ -1,9 +1,8 @@
 def add_one(some_list):
     number = int("".join(map(str, some_list)))
-
     number += 1
-
     return [int(digit) for digit in str(number)]
+
 
 result = add_one([1, 2, 3, 4])
 print(result)
