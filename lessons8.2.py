f7bd1ef7d223824f5790096b402ac9ce8f9403c1
SvetlanaNesmiyan <163138406+SvetlanaNesmiyan@users.noreply.github.com>
2025-01-23 13:24:02 +0000

    lessons8.2.py

--- lessons8.2.py
+++ lessons8.2.py
@@ -1,12 +1,9 @@
 import string
-
 def is_palindrome(text):
     text = text.lower()
-
     filtered_text = ''.join(ch for ch in text if ch.isalnum())
-
     return filtered_text == filtered_text[::-1]
 
-result = is_palindrome('A man, a plan, a canal: Panama')
 
+result = is_palindrome('A man, a plan, a canal: Panama')
 print(result)
