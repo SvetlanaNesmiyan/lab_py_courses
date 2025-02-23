70d7da6a3f976f99affd5bd2fc1e2a8987391760
SvetlanaNesmiyan <163138406+SvetlanaNesmiyan@users.noreply.github.com>
2025-02-23 13:30:17 +0000

    lessons14.1.py

---
+++ lessons14.1.py
@@ -0,0 +1,78 @@
+class GroupLimitError(Exception):
+
+    def __init__(self, message="Перевищено ліміт студентів у групі"):
+        super().__init__(message)
+
+class Human:
+
+    def __init__(self, gender, age, first_name, last_name):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}, {self.age} years old"
+
+class Student(Human):
+
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        super().__init__(gender, age, first_name, last_name)
+        self.record_book = record_book
+
+    def __str__(self):
+        return f"{super().__str__()}, Record Book: {self.record_book}"
+
+class Group:
+
+    MAX_STUDENTS = 10
+
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+        if len(self.group) >= self.MAX_STUDENTS:
+            raise GroupLimitError(f"Cannot add more than {self.MAX_STUDENTS} students to the group {self.number}")
+        self.group.add(student)
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
+
+    def delete_student(self, last_name):
+        student = self.find_student(last_name)
+        if student:
+            self.group.remove(student)
+
+    def __str__(self):
+        all_students = '\n'.join(str(student) for student in self.group)
+        return f'Number: {self.number}\n{all_students}'
+
+
+st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+gr = Group('PD1')
+gr.add_student(st1)
+gr.add_student(st2)
+print(gr)
+
+assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
+assert gr.find_student('Jobs2') is None, 'Test2'
+assert isinstance(gr.find_student('Jobs'), Student) is True, 'Метод поиска должен возвращать экземпляр'
+
+gr.delete_student('Taylor')
+print(gr)  # Only one student
+
+gr.delete_student('Taylor')  # No error!
+
+
+try:
+    gr = Group('PD1')
+    for i in range(11):  # Додаємо 11 студентів
+        gr.add_student(Student('Male', 20 + i, f'Name{i}', f'Surname{i}', f'AN14{i}'))
+except GroupLimitError as e:
+    print(f"Exception caught: {e}")
+    
